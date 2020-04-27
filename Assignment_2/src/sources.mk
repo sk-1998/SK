982864e9570efd9ea6b44cbae185c1dc8aaac9ea
saikiran <saikiranbehara17ex@student.mes.ac.in>
2020-04-27 18:27:40 +0530

    made changes in makefile

--- Assignment_2/src/sources.mk
+++ Assignment_2/src/sources.mk
@@ -10,14 +10,32 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
+ifeq ($(PLATFORM),HOST)
 SOURCES =	\
 	./main.c \
 	./memory.c
 
+else
+SOURCES =	\
+	./main.c \
+	./memory.c \
+	./interrupts_msp432p401r_gcc.c \
+	./startup_msp432p401r_gcc.c \
+	./system_msp432p401r.c
+
+endif
+
 # Add your include paths to this variable
-INCLUDES = \
-	 -I ../include/common \
-	-I ../include/CMSIS \
-	-I ../include/msp432
+ifeq ($(PLATFORM),HOST)
+INCLUDES =        \
+	 -I ../include/common 
 
+	
 
+else 
+INCLUDES =	\
+	-I ../include/common \
+	-I ../include/msp432 \
+	-I ../include/CMSIS 
+ 
+endif
